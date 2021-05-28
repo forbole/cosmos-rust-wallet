dd7e4528e03b2a6a1587d774ebe6c9610175e7d3
Manuel <manuel.turetta94@gmail.com>
2021-05-28 17:48:07 +0200

    crw-preferences: Added a wrapper to expose the Preference trait to a js application

--- packages/crw-preferences/src/lib.rs
+++ packages/crw-preferences/src/lib.rs
@@ -1,2 +1,10 @@
+#[macro_use]
+extern crate cfg_if;
+
 mod cypher;
-mod storage;
+mod io;
+pub mod preferences;
+pub mod unencrypted;
+
+#[cfg(feature = "wasm-bindgen")]
+pub mod wasm;
---
+++ packages/crw-preferences/src/wasm.rs
@@ -0,0 +1,66 @@
+//! Module that provides a wrapper to expose a [Preferences] to a js application.
+
+extern crate bindgen as wasm_bindgen;
+
+use crate::preferences::{Preferences, PreferencesError};
+use crate::unencrypted::UnencryptedPreferences;
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen(js_name = Preferences)]
+pub struct PreferencesWrapper {
+    container: Box<dyn Preferences>,
+}
+
+#[wasm_bindgen(js_class = Preferences)]
+impl PreferencesWrapper {
+    pub fn get_i32(&self, key: &str) -> Option<i32> {
+        self.container.get_i32(key)
+    }
+
+    pub fn put_i32(&mut self, key: &str, value: i32) -> Result<(), JsValue> {
+        Ok(self.container.put_i32(key, value)?)
+    }
+
+    pub fn get_bool(&self, key: &str) -> Option<bool> {
+        self.container.get_bool(key)
+    }
+
+    pub fn put_bool(&mut self, key: &str, value: bool) -> Result<(), JsValue> {
+        Ok(self.container.put_bool(key, value)?)
+    }
+
+    pub fn get_string(&self, key: &str) -> Option<String> {
+        self.container.get_str(key)
+    }
+
+    pub fn put_string(&mut self, key: &str, value: &str) -> Result<(), JsValue> {
+        Ok(self.container.put_str(key, value.to_owned())?)
+    }
+
+    pub fn clear(&mut self) {
+        self.container.clear()
+    }
+
+    pub fn erase(&mut self) {
+        self.container.erase();
+    }
+
+    pub fn save(&self) -> Result<(), JsValue> {
+        Ok(self.container.save()?)
+    }
+}
+
+#[wasm_bindgen(js_name = "preferences")]
+pub fn preferences(name: &str) -> Result<PreferencesWrapper, JsValue> {
+    UnencryptedPreferences::new(name)
+        .map(|container| PreferencesWrapper {
+            container: Box::new(container),
+        })
+        .map_err(JsValue::from)
+}
+
+impl From<PreferencesError> for JsValue {
+    fn from(e: PreferencesError) -> Self {
+        JsValue::from(e.to_string())
+    }
+}
