13569f9bdde0198dafe52153961aad47886b7b98
Manuel <manuel.turetta94@gmail.com>
2021-07-09 14:09:22 +0200

    crw-preferences: Fixed wasm bindings

--- packages/crw-preferences/src/wasm.rs
+++ packages/crw-preferences/src/wasm.rs
@@ -15,35 +15,43 @@ pub struct PreferencesWrapper {
 
 #[wasm_bindgen(js_class = Preferences)]
 impl PreferencesWrapper {
+    #[wasm_bindgen(js_name = "getI32")]
     pub fn get_i32(&self, key: &str) -> Option<i32> {
         self.container.get_i32(key)
     }
 
+    #[wasm_bindgen(js_name = "putI32")]
     pub fn put_i32(&mut self, key: &str, value: i32) -> Result<(), JsValue> {
         Ok(self.container.put_i32(key, value)?)
     }
 
+    #[wasm_bindgen(js_name = "getStr")]
     pub fn get_str(&self, key: &str) -> Option<String> {
         self.container.get_str(key)
     }
 
+    #[wasm_bindgen(js_name = "putStr")]
     pub fn put_str(&mut self, key: &str, value: &str) -> Result<(), JsValue> {
         Ok(self.container.put_str(key, value.to_owned())?)
     }
 
+    #[wasm_bindgen(js_name = "getBool")]
     pub fn get_bool(&self, key: &str) -> Option<bool> {
         self.container.get_bool(key)
     }
 
+    #[wasm_bindgen(js_name = "putBool")]
     pub fn put_bool(&mut self, key: &str, value: bool) -> Result<(), JsValue> {
         Ok(self.container.put_bool(key, value)?)
     }
 
-    fn get_bytes(&self, key: &str) -> Option<Vec<u8>> {
+    #[wasm_bindgen(js_name = "getBytes")]
+    pub fn get_bytes(&self, key: &str) -> Option<Vec<u8>> {
         self.container.get_bytes(key)
     }
 
-    fn put_bytes(&mut self, key: &str, value: Vec<u8>) -> Result<(), JsValue> {
+    #[wasm_bindgen(js_name = "putBytes")]
+    pub fn put_bytes(&mut self, key: &str, value: Vec<u8>) -> Result<(), JsValue> {
         Ok(self.container.put_bytes(key, value)?)
     }
 
@@ -61,8 +69,8 @@ impl PreferencesWrapper {
 }
 
 #[wasm_bindgen]
-pub fn exist(name: &str) {
-    preferences::exist(name);
+pub fn exist(name: &str) -> bool {
+    preferences::exist(name)
 }
 
 #[wasm_bindgen]
