14da127ee99852ad7bd1e8b21f73d8daf4dd488f
fragilegirl <zenaoxana85@gmail.com>
2022-08-23 04:49:01 -0400

    fixed webpack compile error added experimental feature for version 5

--- examples/browser/webpack.config.js
+++ examples/browser/webpack.config.js
@@ -7,6 +7,10 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
     filename: "bootstrap.js",
   },
+  experiments: {
+    asyncWebAssembly: true,
+    syncWebAssembly: true,
+  },
   mode: "development",
   plugins: [
     new CopyWebpackPlugin(['index.html'])
